e71087efcb3d15705e9424a814e8ebd26df840e4
helloimda <149202694+helloimda@users.noreply.github.com>
2024-01-20 23:57:07 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+#include<string>
+using namespace std;
+
+int main() {
+
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	string str;
+	int a;
+	cin >> str >> a;
+
+	cout << str[a - 1];
+	
+}
