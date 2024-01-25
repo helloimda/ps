124fb09f492d71b29ca21353c30b3100541cf852
helloimda <149202694+helloimda@users.noreply.github.com>
2024-01-25 11:57:41 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string str, ans;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    string str;
+    cin >> str;
+    cout << str.size();
+}
