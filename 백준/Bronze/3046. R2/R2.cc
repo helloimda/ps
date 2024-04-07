4ff0d4bf0ac87570d38ffe1b9e2123c80190d5da
helloimda <149202694+helloimda@users.noreply.github.com>
2024-04-07 22:42:43 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<queue>
+#include<climits>
+using namespace std;
+
+
+int main(void) {
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	int a, b;
+	cin >> a >> b;
+	cout << b * 2 - a;
+}
